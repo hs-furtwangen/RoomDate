83b6fe03e243bb4908ab1d156c265cb682f1c8b9
Alexander Scheurer <mail@aspepex.net>
2018-06-09 16:20:37 +0100

    Basic functionality

---
+++ Assets/Scripts/RoomTimer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoomTimer : MonoBehaviour {
+
+    public float RoomBaseTimer;
+    public string NextScene;
+
+    public bool RunTimer;
+
+    private bool _validtimer;
+
+    private void Start()
+    {
+        if (RoomBaseTimer > 0 && NextScene != "")
+        {
+            _validtimer = true;
+        }
+        else
+        {
+            _validtimer = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (RunTimer && _validtimer)
+        {
+            if (RoomBaseTimer <= 0)
+            {
+                this.gameObject.GetComponent<SceneController>().LoadScene(NextScene);
+            }
+            else
+            {
+                RoomBaseTimer -= Time.deltaTime;
+            }
+        }
+	}
+}
--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -5,16 +5,19 @@ public class SceneController : MonoBehaviour {
 
     public void LoadScene(string name)
     {
+        Debug.Log("Loading scene " + name);
         SceneManager.LoadScene(name);
     }
 
     public void LoadScene(int index)
     {
+        Debug.Log("Loading scene with index " + index);
         SceneManager.LoadScene(index);
     }
 
     public void Exit()
     {
+        Debug.Log("Bye!");
         Application.Quit();
     }
 }
